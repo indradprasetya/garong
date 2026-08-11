46de4d44dc45c0b8051b79d74205e6ed0de56d59
indradprasetya <indrayanadian7@gmail.com>
2026-08-11 09:17:26 +0800

    Initial Commit

---
+++ garong/garongApp.swift
@@ -0,0 +1,17 @@
+//
+//  garongApp.swift
+//  garong
+//
+//  Created by Indrayana Dian Prasetya on 11/08/26.
+//
+
+import SwiftUI
+
+@main
+struct garongApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
