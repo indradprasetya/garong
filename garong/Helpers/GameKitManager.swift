8011a28dbf652088177849ae1b5da1cfb69253ac
Muhammad Bintang Al-Fath <alfathbintangmuhammad@gmail.com>
2026-09-04 16:05:50 +0800

    fix: wrong identifier on gamekit

--- garong/Helpers/GameKitManager.swift
+++ garong/Helpers/GameKitManager.swift
@@ -25,14 +25,14 @@ public final class GameKitManager: NSObject, ObservableObject {
     // MARK: - Game Center Identifiers
     public struct Identifiers {
         // Leaderboards
-        public static let leaderboardTotalStars = "com.vidya.kinario.leaderboard.total_stars"
-        public static let leaderboardStoriesCompleted = "com.vidya.kinario.leaderboard.stories_completed"
+        public static let leaderboardTotalStars = "com.vidya.garong.leaderboard.total_stars"
+        public static let leaderboardStoriesCompleted = "com.vidya.garong.leaderboard.stories_completed"
 
         // Achievements
-        public static let achievementFirstStory = "com.vidya.kinario.achievement.first_story"
-        public static let achievementAllStories = "com.vidya.kinario.achievement.all_stories"
-        public static let achievementThreeStars = "com.vidya.kinario.achievement.three_stars"
-        public static let achievementMastery = "com.vidya.kinario.achievement.mastery"
+        public static let achievementFirstStory = "com.vidya.garong.achievement.first_story"
+        public static let achievementAllStories = "com.vidya.garong.achievement.all_stories"
+        public static let achievementThreeStars = "com.vidya.garong.achievement.three_stars"
+        public static let achievementMastery = "com.vidya.garong.achievement.mastery"
     }
 
     public override init() {
